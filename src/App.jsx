6d3834cd20c4454845990edb7dd94c52be522d60
Gavinus <96060707+Gavinus@users.noreply.github.com>
2022-04-15 11:04:16 +0200

    re push menu and resolved conflit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-// import { useState, useEffect } from "react";
+import React from 'react';
+import { useState, useEffect } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-// import BarLogin from "./components/BarLogin";
-import Landing from "./components/Landing";
 import Menu from "./components/Menu";
 
-function App() {
+
+
+
+function App() {    
+
+
+
   return (
+<<<<<<< Updated upstream
     <div className="h-screen bg-[#B2915F] App">
       <nav>
         <Landing />
@@ -18,6 +22,11 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/menu" element={<Menu />} />
       </Routes>
+=======
+    <div className="App">
+      <Menu />
+
+>>>>>>> Stashed changes
     </div>
   );
 }
