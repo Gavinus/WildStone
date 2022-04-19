1a64b43c6d288de97305a3ed9896bd7718e25d01
Gavinus <96060707+Gavinus@users.noreply.github.com>
2022-04-19 15:08:54 +0200

    upgrade menu and some details

---
+++ src/components/Battleground.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Battleground = () => {
+  return <h1>BATTLERGROUND</h1>;
+};
+
+export default Battleground;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,jsx}"],
   // ...
+  theme: {
+    fontFamily: {
+        belwe: ["belwe", "serif"],
+      },
+      extend: {
+        width: {
+          '100': '10%',
+        },
+      }
+  },
+plugins: [],
 };
