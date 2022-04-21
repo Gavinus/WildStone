7fe71fa88675e61ccd0fdff3604d642bb904bb6d
jcfauquenot <jcfauquenot@gmail.com>
2022-04-21 09:56:33 +0200

    add login in landing page

--- src/components/BarLogin.jsx
+++ src/components/BarLogin.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+// import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const BarLogin = () => {
+  const userName = sessionStorage.user;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    sessionStorage.clear();
+    navigate("/");
+  };
+
   return (
     <div className="flex justify-between h-14 bg-[url(https://cdn.discordapp.com/attachments/960537598365958174/961183931451981864/unknown.png)]">
       <div className="flex">
-        <p className="mt-3 ml-8 text-white ">UserName</p>
-        <p className="mt-3 ml-12 text-white ">Level</p>
+        <p className="mt-3 ml-8 text-white ">{userName}</p>
+        {/* <p className="mt-3 ml-12 text-white ">Level</p> */}
       </div>
-      <img
-        className="w-8 h-8 mt-3 mr-4"
+      <input
+        type="image"
+        className="w-8 h-8 mt-3 mr-4 hover:scale-150"
         src="https://cdn.discordapp.com/attachments/960537598365958174/961329189246619658/icone_1.png"
         alt="icone du joueur"
+        onClick={handleClick}
+        autoComplete="on"
       />
     </div>
   );
