0c4d940e0b1d460847426efbf8064682db5fcae2
jcfauquenot <jcfauquenot@gmail.com>
2022-05-10 22:41:04 +0200

    label franch in memmory game

--- src/components/Memory.jsx
+++ src/components/Memory.jsx
@@ -85,14 +85,14 @@ const Memory = () => {
             ))}
           </div>
           <h2 className="mx-12 text-3xl font-bold text-yellow-500">
-            Number of turns : {turns}
+            Nombre de tours : {turns}
           </h2>
           <button
             className="px-4 py-2 text-2xl font-bold text-white bg-blue-500 rounded-full mx-96 hover:bg-blue-500"
             type="button"
             onClick={melangeCards}
           >
-            Start New Game
+            Nouvelle Partie
           </button>
         </div>
       </div>
